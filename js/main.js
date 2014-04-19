d17b56c8a8f3aa5389f236144f1d95cc99d248e5
Ray Walters <ray.walters@gmail.com>
2014-04-19 02:28:11 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+require.config({
+    paths: {
+                //tries to load jQuery from Google's CDN first and falls back
+                //to load locally
+        "jquery": ["http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
+                    "libs/jquery/src/jquery"],
+        "underscore": "libs/underscore/underscore",
+        "backbone": "libs/backbone/backbone"
+    },
+    shim: {
+        "backbone": {
+            deps: ["jquery", "underscore"],
+            exports: "Backbone"
+        }
+    },
+    waitSeconds: 10
+});
+require(['jquery', 'underscore', 'backbone', 'app'], function(jquery, _, Backbone, App){
+    new App;
+});
