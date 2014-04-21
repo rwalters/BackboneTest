d0cc2102814f88c06d4bf48d224a6110c0684073
Ray Walters <ray.walters@gmail.com>
2014-04-21 02:25:52 +0000

    Ugly, but it lists the data.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 define(["backbone"], function(Backbone){
   var MimeType = Backbone.Model.extend({ });
+  var MimeTypeView = Backbone.View.extend({
+    template: _.template('<tr><td><%= type %></td><td><%= fileExtension %></td><td><%= description %></td><td><%= supported %></td></tr>'),
+    render: function(){
+      this.$el.html(this.template(this.model.attributes));
+      return this;
+    }
+  });
   var MimeTypes = Backbone.Collection.extend({
     parse: function(response){
       return response.mimeTypeFullList;
@@ -7,12 +14,34 @@ define(["backbone"], function(Backbone){
     url: 'https://api.qa.mimedia.com/2.0/mimetypesfull/supported/list/',
     model: MimeType
   });
-
-  var App = Backbone.View.extend({
+  var MimeTypesView = Backbone.View.extend({
     initialize: function(){
-      var mimeTypes = new MimeTypes;
-        mimeTypes.fetch({ success : function(model, err) { console.log(mimeTypes.at(0).attributes) } });
-      }
+      this.collection.on('reset', this.render, this);
+    },
+    render: function(){
+      this.addAll();
+      return this;
+    },
+    addAll: function(){
+      this.$el.html("<table><th><td>Type</td><td>File Extension</td><td>Description</td><td>Supported Format</td></th>");
+      this.collection.forEach(this.addOne, this);
+      this.$el.append("</table>");
+    },
+    addOne: function(mimeType){
+      var mimeTypeView = new MimeTypeView({model: mimeType});
+      this.$el.append(mimeTypeView.render().el);
+    }
   });
+
+  var App = function() {
+    var mimeTypes = new MimeTypes;
+    var mimeTypesView = new MimeTypesView({collection: mimeTypes});
+    mimeTypes.fetch({
+      success : function(model, err) {
+        $('#app').html(mimeTypesView.render().el);
+      }
+    });
+  }
+
   return App;
 });
